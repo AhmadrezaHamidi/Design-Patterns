d76417e906bfb77d9ab93e560defe442e72a83fb
Ahmadreza Hamid Fard <61118112+AhmadrezaHamidi@users.noreply.github.com>
2024-07-22 17:31:59 +0330

    problemleetCode

--- AhmadTests/ReportBuilder/obj/Debug/net8.0/ReportBuilder.AssemblyInfo.cs
+++ AhmadTests/ReportBuilder/obj/Debug/net8.0/ReportBuilder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReportBuilder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+670a221601b174314c626fab644bb86ba321d9a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48dedb80bf86b932548b4de51d24b73bc30b39eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReportBuilder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReportBuilder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ LeetCode/ConsoleApp1/Domin/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Domin
+{
+    public static class Calculator
+    {
+        public static int SumTOArrayes(int[] numbers, int[] number2)
+        {
+            try
+            {
+                if (numbers.Length != number2.Length)
+                    throw new ArgumentException();
+
+                var numberOne = Convert.ToInt32(string.Join(null, numbers.Select(number => number.ToString())));
+                var numberTwo = Convert.ToInt32(string.Join(null, numbers.Select(number => number.ToString())));
+
+                return numberOne + numberTwo;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+          
+        }
+    }
+}
